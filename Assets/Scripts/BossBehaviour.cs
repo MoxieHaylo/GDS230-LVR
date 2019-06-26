c8e36b66938010356a0d05bb44e7abfd5a326455
Hayley <Wilson>
2019-06-26 11:14:40 +1000

    Set up project
    Started working on orb scripts
    Added Oculus assets

---
+++ Assets/Scripts/BossBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBehaviour : MonoBehaviour
+{
+    // boss health
+    // attack 
+    // - pull from list (at random)
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //
+    }
+}
---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+
+    public List<BlueOrb> blueOrbPool;
+    public BlueOrb blueOrbPrefab;
+    public Vector3 spawnPosition;
+
+
+
+}
