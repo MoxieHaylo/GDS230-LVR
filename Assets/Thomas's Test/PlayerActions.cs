4e2cdee9fc25ac4448d5e928dae4dadd1724c4b1
Thomas <1013454@student.sae.edu.au>
2019-07-09 16:20:02 +1000

    Turret Action Testing

---
+++ Assets/Thomas's Test/PlayerActions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    public GameObject redOrb;
+    public GameObject greenOrb;
+    public GameObject yellowOrb;
+    public GameObject blueOrb;
+    private int redCharge;
+    private int greenCharge;
+    private int yellowCharge;
+    private int blueCharge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        redCharge = 0;
+        greenCharge = 0;
+        yellowCharge = 0;
+        blueCharge = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Thomas's Test/TurretLogic.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretLogic : MonoBehaviour
+{
+
+    public GameObject[] waypoints;
+    int myCurrentWaypoint;
+    public GameObject myProjectile;
+    public bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMoving = true;
+        myCurrentWaypoint = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+       
+    }
+
+    private void FixedUpdate()
+    {
+        if (isMoving == true)
+        {
+            MoveToWaypoint();
+        }
+
+        
+
+        if(isMoving == false)
+        {
+            shootProjectile();
+        }
+    }
+
+    void MoveToWaypoint()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, waypoints[myCurrentWaypoint].transform.position, 0.15f);
+
+        if (Vector3.Distance(this.transform.position, waypoints[myCurrentWaypoint].transform.position) < 0.01f)
+        {
+            myCurrentWaypoint = Random.Range(0, waypoints.Length);
+        }
+    }
+
+    
+
+    void shootProjectile()
+    {
+        GameObject newProjectile = Instantiate(myProjectile, this.transform.position, this.transform.rotation);
+
+        isMoving = true;
+    }
+    
+}
