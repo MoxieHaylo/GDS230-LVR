bdeab2ebbb5a098958f2f48e8194b2b9fd786269
Hayley Wilson <hayley@moxiehaylo.games>
2019-08-06 10:40:05 +1000

    Cleaned up your mess ya filthy animals <3
    Also adjusted post

---
+++ Assets/Scripts/TestPlayerShot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerShot : MonoBehaviour
+{
+    GameObject myTarget;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTarget = GameObject.FindGameObjectWithTag("Boss");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, myTarget.transform.position, 0.1f);
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Boss")
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    
+}
---
+++ Assets/Scripts/TestShieldLogic.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestShieldLogic : MonoBehaviour
+{
+    
+    public OrbType targetType;
+    TestDeviceLogic charge;
+    //public GameObject targetOrb;
+    //public GameObject[] wrongOrbs;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charge = GetComponentInParent<TestDeviceLogic>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if(coll.GetComponent<ProjectileActions>())
+        {
+            if(coll.GetComponent<ProjectileActions>().type == targetType)
+            {
+                
+                charge.GainCharge(20);
+                Destroy(coll.gameObject);
+            }
+            else
+            {
+                charge.LoseCharge(-20);
+                Destroy(coll.gameObject);
+            }
+        }
+        
+
+        
+    }
+
+    
+
+    
+
+
+}
